4b9cad70bf3df499f5bcd5481c6adf405f511286
SAREGH <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2024-06-16 08:35:02 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@
 
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
     name='box overlaps',
-    ext_modules=cythonize('./utils/box_overlaps.pyx')
+    ext_modules=cythonize('./utils/box_overlaps.pyx'),
+    include_dirs=[numpy.get_include()]
 )
 
 # solution for potential error related to numpy/arrayobject.h
